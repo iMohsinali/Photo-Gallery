27f6773159d2e0347963aad92a1dbf401708f9d2
iMohsinali <87361294+iMohsinali@users.noreply.github.com>
2021-09-12 16:24:55 +0500

    2nd Commit

--- src/comps/hook/useStorge.js
+++ src/comps/hook/useStorge.js
@@ -1,9 +1,25 @@
 import React, { useState } from 'react'
 import { projectstore } from './firebase/config'
 const useStorge = (file) => {
-    const [progress, setprogress] = useState()
-    const [error, seterror] = useState()
-    const [url, seturl] = useState()
+    const [progress, setprogress] = useState(0)
+    const [error, seterror] = useState(null)
+    const [url, seturl] = useState(null)
+    useEffect(() => {
+        //reference
+        const storgeref = projectstore.ref(file.name)
+        storgeref.put(file).on('state_changed'), snap(() => {
+            let percentage = (snap.bytesTransferred / snap.totalBytes) * 100
+            setprogress(percentage)
+        }, (err) => {
+            seterror(err)
+        }, async () => {
+            const url = await storgeref.getDownlaodURL()
+            seturl(url)
+        })
+        return {
+            progress, url, error
+        }
+    }, [file])
     return (
         <div>
 
