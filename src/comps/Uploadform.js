085926c30cebd673799ceb580c303fa29e628af5
iMohsinali <87361294+iMohsinali@users.noreply.github.com>
2021-09-12 16:11:28 +0500

    first commit

--- src/comps/Uploadform.js
+++ src/comps/Uploadform.js
@@ -1,11 +1,33 @@
-import React from 'react'
-const changehandler = (e) => {
-    console.log("Chnage has been happen")
-}
+import React, { useState } from 'react'
+
 const Uploadform = () => {
+    const [file, setfile] = useState(null)
+    const [error, seterror] = useState(null)
+    const types = ['image/png', 'image/jpg']
+    const changehandler = (e) => {
+        let selected = e.target.files[0]
+        if (selected && types.includes(selected.type)) {
+            setfile(selected)
+            seterror('')
+        }
+        else {
+            setfile(null)
+            seterror('please select an image type of png/jpg')
+        }
+    }
     return (
         <form>
             <input type='file' onChange={changehandler} />
+            <div className='output'>
+                {
+                    error && <div className='error'>{error} </div>
+
+                }
+                {
+                    file && <div className='files'>{file.name} </div>
+
+                }
+            </div>
         </form>
     )
 }
---
+++ src/comps/hook/useStorge.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { projectstore } from './firebase/config'
+const useStorge = (file) => {
+    const [progress, setprogress] = useState()
+    const [error, seterror] = useState()
+    const [url, seturl] = useState()
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default useStorge
